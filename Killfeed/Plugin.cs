3e87ec37aa0206de568ba5fb9373db350df1ed31
SakuraaGT <73008150+SakuraaDevelopment@users.noreply.github.com>
2023-05-28 13:46:05 -0700

    Update Plugin.cs

--- Killfeed/Plugin.cs
+++ Killfeed/Plugin.cs
@@ -1,39 +1,91 @@
-﻿using BepInEx;
+using BepInEx;
 using UnityEngine;
 using HoneyLib.Events;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Killfeed
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        string killFeed = "";
-        string latestTag;
+        private class KillFeedEntry
+        {
+            public string Text;
+            public float ExpirationTime;
+        }
+
+        private List<KillFeedEntry> killFeedEntries = new List<KillFeedEntry>();
+        private string latestTag;
+
+        private const float FeedEntryLifetime = 60f;
+        private const int TextSize = 22;
+
+        private GUIStyle redStyle;
+        private GUIStyle whiteStyle;
+        private GUIStyle cyanStyle;
 
         void Start()
         {
             Events.InfectionTagEvent += InfectionTagEvent;
+
+            redStyle = new GUIStyle();
+            redStyle.normal.textColor = Color.red;
+            redStyle.fontSize = TextSize;
+
+            whiteStyle = new GUIStyle();
+            whiteStyle.normal.textColor = Color.white;
+            whiteStyle.fontSize = TextSize;
+
+            cyanStyle = new GUIStyle();
+            cyanStyle.normal.textColor = Color.cyan;
+            cyanStyle.fontSize = TextSize;
+        }
+
+        void Update()
+        {
+            RemoveExpiredEntries();
         }
 
         void OnGUI()
         {
-            GUI.Box(new Rect(Screen.width - 320, 20, 300, 200), killFeed);
-            if (GUI.Button(new Rect(Screen.width - 70, 235, 50, 30), "Clear"))
+            float feedWidth = 400f;
+            float feedHeight = 200f;
+            float margin = 1f;
+
+            float xPos = Screen.width - feedWidth - margin;
+            float yPos = margin;
+
+            GUILayout.BeginArea(new Rect(xPos, yPos, feedWidth, feedHeight));
+            GUILayout.FlexibleSpace();
+
+            foreach (KillFeedEntry entry in killFeedEntries)
             {
-                killFeed = "";
+                string[] names = entry.Text.Split(new[] { " tagged " }, System.StringSplitOptions.None);
+                string taggerName = names[0];
+                string taggedName = names[1];
+                GUILayout.BeginHorizontal();
+
+                GUILayout.Label(taggerName, redStyle);
+                GUILayout.Label(" tagged ", whiteStyle);
+                GUILayout.Label(taggedName, cyanStyle);
+
+                GUILayout.EndHorizontal();
             }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndArea();
         }
-        
+
+
         void InfectionTagEvent(object sender, InfectionTagEventArgs e)
         {
             latestTag = e.taggingPlayer.NickName + " tagged " + e.taggedPlayer.NickName;
-            
-            if (Regex.Matches(killFeed, "\n").Count > 10)
-            {
-                killFeed = latestTag;
-            }
-            else killFeed = killFeed + "\n" + latestTag;
+            killFeedEntries.Add(new KillFeedEntry { Text = latestTag, ExpirationTime = Time.time + FeedEntryLifetime });
+        }
+
+        private void RemoveExpiredEntries()
+        {
+            killFeedEntries.RemoveAll(entry => entry.ExpirationTime <= Time.time);
         }
     }
 }
